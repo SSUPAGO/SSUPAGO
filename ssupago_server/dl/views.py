3e7261b37d7faba2f0daaf42471005f63524e90a
Eundms <dms011221@gmail.com>
2022-08-29 22:16:01 +0900

    fix: base url of dl

--- ssupago_server/dl/views.py
+++ ssupago_server/dl/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 
 @api_view(['POST'])
@@ -16,6 +15,6 @@ def post_api(request):
         return Response('Error', status = status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET'])
-def get_api():
+def get_api(request):
     return Response('GET!!!', status = status.HTTP_200_OK)
     
