271424878d4e0fd8372971637671097028bed458
Eundms <dms011221@gmail.com>
2022-09-01 18:11:12 +0900

    style

--- ssupago_server/dl/views.py
+++ ssupago_server/dl/views.py
@@ -1,5 +1,5 @@
+import sys
 from django.shortcuts import render
-
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,8 +9,9 @@ from rest_framework import status
 def post_api(request):
     if request.data :
         # 모델 Import & 예측 요청
-        
-        return Response("You're looking at question",status=status.HTTP_200_OK)
+        sentence ="이런 바보멍청이 같은개"
+        predict_res=model_run.predict(sentence)
+        return Response("You're looking at question"+predict_res,status=status.HTTP_200_OK)
     else :
         return Response('Error', status = status.HTTP_400_BAD_REQUEST)
     
